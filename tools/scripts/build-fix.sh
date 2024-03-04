1b10d15f074e4caea62a8bf2ced940e13aca1495
Alex <aesshoferi@gmail.com>
2024-03-04 17:13:26 +0330

    fix: fix vercel builds

---
+++ tools/scripts/build-fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# generate envs.js file and run the app
+dotenv \
+  -v NEXT_PUBLIC_GIT_COMMIT_SHA=$(git rev-parse --short HEAD) \
+  -v NEXT_PUBLIC_GIT_TAG=$(git describe --tags --abbrev=0) \
+  -e .env.secrets \
+  -e .env.development.local \
+  -e .env.local \
+  -e .env.development \
+  -e .env \
+  -- bash -c './deploy/scripts/make_envs_script.sh && next build' |
+pino-pretty
