58f7071d165bea781a3d176d2f61ad7f931d3839
Alex <aesshoferi@gmail.com>
2024-03-05 10:05:55 +0330

    fix: fix vercel builds

--- configs/app/app.ts
+++ configs/app/app.ts
@@ -2,13 +2,14 @@ import { getEnvValue } from './utils';
 
 const appPort = getEnvValue('NEXT_PUBLIC_APP_PORT');
 const appSchema = getEnvValue('NEXT_PUBLIC_APP_PROTOCOL');
-const appHost = getEnvValue('NEXT_PUBLIC_VERCEL_URL') || getEnvValue('NEXT_PUBLIC_APP_HOST');
-const baseUrl = [
-  appSchema || 'https',
-  '://',
-  appHost,
-  appPort && ':' + appPort,
-].filter(Boolean).join('');
+const appHost =
+  (getEnvValue('NEXT_PUBLIC_VERCEL_ENV') === 'preview' ?
+    getEnvValue('NEXT_PUBLIC_VERCEL_URL') :
+    getEnvValue('NEXT_PUBLIC_VERCEL_BRANCH_URL')) ||
+  getEnvValue('NEXT_PUBLIC_APP_HOST');
+const baseUrl = [ appSchema || 'https', '://', appHost, appPort && ':' + appPort ]
+  .filter(Boolean)
+  .join('');
 const isDev = getEnvValue('NEXT_PUBLIC_APP_ENV') === 'development';
 
 const app = Object.freeze({
